9fc35b393679c704673274c995f251f94b9a74ae
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-06-28 17:52:32 +0200

    Using WebView

---
+++ app/src/main/java/com/example/marwa/worldnews/WebViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.marwa.worldnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("news");
+        WebView web = (WebView) findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl(url);
+    }
+}
