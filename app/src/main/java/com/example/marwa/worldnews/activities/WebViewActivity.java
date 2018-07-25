0222de60a4ad41552d7938d46b74e6aafbcd7e36
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-07-26 00:04:47 +0200

    Add Share feature

--- app/src/main/java/com/example/marwa/worldnews/activities/WebViewActivity.java
+++ app/src/main/java/com/example/marwa/worldnews/activities/WebViewActivity.java
@@ -3,12 +3,17 @@ package com.example.marwa.worldnews.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.example.marwa.worldnews.R;
+import com.example.marwa.worldnews.Utility;
 
 public class WebViewActivity extends AppCompatActivity {
+
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,34 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         Intent intent = getIntent();
-        String url = intent.getStringExtra("news");
+        url = intent.getStringExtra("news");
         WebView web = (WebView) findViewById(R.id.webView);
         web.setWebViewClient(new WebViewClient());
         web.loadUrl(url);
     }
+
+    /**
+     * Initialize the contents of the Activity's options menu.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/share.xml file.
+        getMenuInflater().inflate(R.menu.share, menu);
+        return true;
+    }
+
+    /**
+     * Setup the specific action that occurs when any of the items in the Options Menu are selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.share) {
+            Utility.shareNewsStory(this,url);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
