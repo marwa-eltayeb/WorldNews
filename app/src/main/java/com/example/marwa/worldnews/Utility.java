59db8a5a7f53dafb75300c380c4865f7ce452156
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-07-26 00:05:36 +0200

    Use Share feature

---
+++ app/src/main/java/com/example/marwa/worldnews/Utility.java
@@ -0,0 +1,43 @@
+package com.example.marwa.worldnews;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Marwa on 7/25/2018.
+ */
+
+public class Utility {
+
+    /**
+     * Share the news story.
+     */
+    public static void shareNewsStory(Context context, String url){
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, url);
+        context.startActivity(Intent.createChooser(sharingIntent,"Share using"));
+    }
+
+
+    // Format the date received from guardian JSON
+    public static String formatDate(String currentDate) {
+        // This is the time format from guardian JSON "2017-10-29T06:00:20Z"
+        // will be changed to 29-10-2017 8pm format
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        try {
+            Date newDate = format.parse(currentDate);
+            format = new SimpleDateFormat("dd-MM-yyyy, h:mm a");
+            currentDate = format.format(newDate);
+        } catch (ParseException e) {
+            Log.e("Adapter", "Problem with parsing the date format");
+        }
+        return currentDate;
+    }
+
+}
