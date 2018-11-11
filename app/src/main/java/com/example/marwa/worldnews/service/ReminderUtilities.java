432d00b02acee837bce67755d5ff7fd96129744a
Marwa-Said <marwa_eltayeb@yahoo.com>
2018-11-11 20:03:59 +0200

    Search for New Stories

--- app/src/main/java/com/example/marwa/worldnews/service/ReminderUtilities.java
+++ app/src/main/java/com/example/marwa/worldnews/service/ReminderUtilities.java
@@ -22,7 +22,7 @@ public class ReminderUtilities {
 
     private static final int REMINDER_INTERVAL_HOURS = 24;
     private static final int REMINDER_INTERVAL_SECONDS = (int) (TimeUnit.HOURS.toSeconds(REMINDER_INTERVAL_HOURS));
-    
+
 
     private static final String REMINDER_JOB_TAG = "read_news_tag";
 
@@ -58,7 +58,7 @@ public class ReminderUtilities {
                  */
                 .setRecurring(true)
                 /** make a window of execution */
-                .setTrigger(Trigger.executionWindow(59, REMINDER_INTERVAL_SECONDS))
+                .setTrigger(Trigger.executionWindow(59, 60))
                 /*
                  * If a Job with the tag with provided already exists, this new job will replace
                  * the old one.
